0517b3d7794f53998b8e44efa0d5c6997e4f3cd8
Dayane Ramos <dayanecode@google.com>
2022-09-11 11:42:49 -0300

    configura o server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const server = express()
+
+const middleware = require("./middleware")
+
+server.use(express.json());
+server.use(middleware.Authenticate)
+
+
+module.exports = server
