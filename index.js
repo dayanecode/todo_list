56720134171e8baf9b1527020d30ddf2a5887a30
Dayane Ramos <dayanecode@gmail.com>
2022-09-16 19:30:37 -0300

    test de pull request

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-
 const server = require("./server")
 const database = require("./database")
 
 
 //POST Criar Tarefas
-server.post("/tarefas", function(req, res) {
+server.post("/tasks", function (req, res) {
     database("tasks").
-        insert(req.body).
-        then(function(data) {
-            res.status(201)
-            res.json(data)
+    insert(req.body).
+    then(function (data) {
+        res.status(201)
+        res.json(data)
     }).
-        catch(function(error){
-            res.status(500)
-            res.json(error)
-        })
+    catch(function (error) {
+        res.status(500)
+        res.json(error)
+    })
 })
 
 //GET Listar Tarefas
-server.get("/tasks", function(req, res) {
+server.get("/tasks", function (req, res) {
     database("tasks").
-        then(function(data) {
-                res.json(data)
-        }).
-        catch(function(error) {
-            res.status(500)
-            res.json(error)
-        })
+    then(function (data) {
+        res.json(data)
+    }).
+    catch(function (error) {
+        res.status(500)
+        res.json(error)
+    })
 })
 
 //PATCH Listar uma tarefa específica
-server.patch("/tarefas/:id", function(req, res) {
+server.patch("/tarefas/:id", function (req, res) {
     //req.params.id
     //res.send(`Eu sou um PATCH em ${req.params.id}`)
     res.status(404)
@@ -38,11 +37,11 @@ server.patch("/tarefas/:id", function(req, res) {
 })
 
 //DELETE 
-server.delete("/tarefas/:id", function(req, res) {
-     res.status(404)
+server.delete("/tarefas/:id", function (req, res) {
+    res.status(404)
     res.send("NOT FOUND")
 })
 
 
 
-server.listen(4000)
+server.listen(3000)
--- migrations/1-create-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tasks (
-    id          INT PRIMARY KEY AUTO_INCREMENT
-    tasks       VARCHAR(255) NOT NULL
-    created_as  
-    comments    VARCHAR(255)     
-    
-
-)
